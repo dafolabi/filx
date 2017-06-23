8a0a88a92ddfcfae941a1007a5692c17a6b486a1
Daniel Afolabi <dafolabi@fb.com>
2017-06-23 14:51:20 -0700

    Added collection view

---
+++ filx/PosterCell.swift
@@ -0,0 +1,13 @@
+//
+//  PosterCell.swift
+//  filx
+//
+//  Created by Daniel Afolabi on 6/23/17.
+//  Copyright © 2017 Daniel Afolabi. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+}
---
+++ filx/SuperheroCell.swift
@@ -0,0 +1,13 @@
+//
+//  SuperheroCell.swift
+//  filx
+//
+//  Created by Daniel Afolabi on 6/22/17.
+//  Copyright © 2017 Daniel Afolabi. All rights reserved.
+//
+
+import UIKit
+
+class SuperheroCell: UICollectionViewCell {
+    
+}
